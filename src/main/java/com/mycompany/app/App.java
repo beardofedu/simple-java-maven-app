fdbb2501abbddc8fa133368d216c3eb7310a5bb1
Matt Desmond <beardofedu@github.com>
2020-05-21 16:15:53 -0400

    Change 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,6 @@ public class App
 
     private final String message = "Hello World!";
 
-    public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
